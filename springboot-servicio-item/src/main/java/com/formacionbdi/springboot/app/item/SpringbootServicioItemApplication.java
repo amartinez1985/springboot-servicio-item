09f70f1ec058f04e804549266448d002ecd19eca
Angel Jesus Martinez Aguirre <zoroastroo@live.com>
2020-03-01 08:03:16 -0600

    Clase 23 Conectar microservicios como clientes Eureka, se agrega banner con mi nombre

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,10 +2,11 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "servicios-productos")
+@EnableEurekaClient
+//@RibbonClient(name = "servicios-productos") Se comenta Ribbon ya que se va a utilizar Eureka Server
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
