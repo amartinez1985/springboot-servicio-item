db86a5c9be7a0c8bb0b5150b05c9a313417e384c
Angel Jesus Martinez Aguirre <zoroastroo@live.com>
2020-03-01 16:50:58 -0600

    Clase 25 Se agrega toleracia de fallos y latencia con Hystrix

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,9 +2,11 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 //@RibbonClient(name = "servicios-productos") Se comenta Ribbon ya que se va a utilizar Eureka Server
 @EnableFeignClients
